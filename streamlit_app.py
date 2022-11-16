611597cfc10ac6cc321624cc02f5b2295c434f41
Robert Castagna <49927397+RobertCastagna@users.noreply.github.com>
2022-11-16 11:38:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("My parent's new diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
+
+fruit_list = pd.read_csv("fruit_macros.csv")
+
+streamlit.dataframe(my_fruit_list)
