fb661f4e7c732d6614261c2e3ea9add66aed069d
Robert Castagna <49927397+RobertCastagna@users.noreply.github.com>
2022-11-16 11:52:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text("Omega 3 & Blueberry Oatmeal")
 fruit_list = pd.read_csv("fruit_macros.csv")
 fruit_list = fruit_list.set_index('Fruit')
 
-fruits_selected = streamlit.multiselect("Pick some fruits: ", list(fruit_list.index), ['Avocados','Strawberries'])
-fruits_to_show = fruit_list.loc[fruits_selected]
+fruits_selected = streamlit.multiselect("Pick some fruits: ", list(fruit_list.index))
+#fruits_to_show = fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruit_list)
