b0ae16c2f598f1bf49562cca8d2ef8b0d6cf0301
Robert Castagna <49927397+RobertCastagna@users.noreply.github.com>
2022-11-17 10:24:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.title("My parent's new diner")
 streamlit.header("Breakfast Menu")
@@ -12,3 +13,9 @@ fruits_selected = streamlit.multiselect("Pick some fruits: ", list(fruit_list.in
 fruits_to_show = fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvoce Fruit Advice!')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon").json()
+streamlit.text(fruityvice_response)
+
