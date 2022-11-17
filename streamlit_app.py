5011307d66fe9f4ffaf1180ef99608637693230b
Robert Castagna <49927397+RobertCastagna@users.noreply.github.com>
2022-11-17 10:29:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,10 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvoce Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon").json()
-streamlit.text(fruityvice_response)
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice).json()
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response)
-
 streamlit.dataframe(fruityvice_normalized)
