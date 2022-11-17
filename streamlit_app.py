5fe0a64da179f737b9b86e8fa0f5dd0761cbfc7f
Robert Castagna <49927397+RobertCastagna@users.noreply.github.com>
2022-11-17 14:51:38 -0500

    Update streamlit_app.py
    sectioning code for flow control

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,15 +18,18 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvoce Fruit Advice!')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice).json()
-
-fruityvice_normalized = pd.json_normalize(fruityvice_response)
-streamlit.dataframe(fruityvice_normalized)
-
-## snowflake data
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get informaton.")
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice).json()
+    fruityvice_normalized = pd.json_normalize(fruityvice_response)
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
 
 ## temporary break
 streamlit.stop()
